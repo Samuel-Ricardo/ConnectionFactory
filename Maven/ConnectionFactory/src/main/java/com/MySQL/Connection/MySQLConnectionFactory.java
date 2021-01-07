7e8c4aac121cf657196fbd7d47f85e841cba910a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-01-07 17:11:25 -0300

    Created: Javadoc - Attributes (MySQLConnectionFactory)

--- Maven/ConnectionFactory/src/main/java/com/MySQL/Connection/MySQLConnectionFactory.java
+++ Maven/ConnectionFactory/src/main/java/com/MySQL/Connection/MySQLConnectionFactory.java
@@ -13,27 +13,163 @@ import java.util.logging.Logger;
 
 /**
  *
- * <P>This class aims to make the connection to the <B>MySQL</B> database in a simple way
+ * <P> A Class to connect at <B> MySQL </B>
  * 
- * @author Samuel
+ * <P> For this, Is used the MySQL Connector java 8.0.22 
+ * 
+ * @author Samuel Ricardo Cabral de Barros
  * @version 1.0.0
  * @since Jan 2021
  */
 public class MySQLConnectionFactory {
     
-    private static String DRIVER; 
+    /**
+    *
+    * <P>Driver location
+    * 
+    * <P>It receives by default: {@link #DEFAULT_DRIVER}, however, it can be <BR>
+    * modified by calling the {@link #getConnection(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)} <BR>
+    * method by passing driver as a parameter
+    * 
+    * @serial  DRIVER
+    * @see #DEFAULT_DRIVER
+    */
+    private static String DRIVER = MySQLConnectionFactory.DEFAULT_DRIVER; 
+    
+    /**
+    *
+    * <P>Database Host
+    * 
+    * <P>if you don't know or use a standard Host, try to use the {@link #DEFAULT_HOST}
+    * 
+    * @serial  HOST
+    * @see #DEFAULT_HOST
+    */
     private static String HOST;
+    
+    /**
+    *
+    * <P>Database Port
+    * 
+    * <P>if you don't know or use a standard Port, try to use the {@link #DEFAULT_PORT}
+    * 
+    * @serial  PORT
+    * @see #DEFAULT_PORT
+    */
     private static String PORT;
+    
+    /**
+    *
+    *<P> name of the database to be connected
+    * 
+    * @serial  DATABASE
+    */
     private static String DATABASE;
+    
+    /**
+    *
+    *<P> URL With  Informations to Connect
+    * 
+    * @serial  URL
+    */
     private static String URL; 
+    
+    /**
+    *
+    * <P>Database User Registred
+    * 
+    * <P>if you don't know or use a standard User, try to use the {@link #DEFAULT_USER}
+    * 
+    * @serial  USER
+    * @see #DEFAULT_USER
+    */
     private static String USER;
+    
+    /**
+    *
+    * <P>Database Password Registred
+    * 
+    * <P>if you don't know or use a standard User, try to use the {@link #DEFAULT_USER}
+    * 
+    * @serial  PASSWORD
+    * @see #DEFAULT_PASSWORD
+    */
     private static String PASSWORD;
+    
+    /**
+    *
+    * {@inheritDoc}
+    * 
+    * @serial  connection
+    * @see #{@link com.mysql.cj.jdbc.ConnectionImpl}
+    */
     private static Connection connection;
     
+    
+    
+    
+    /**
+    *
+    *<P> Default Database User Registred
+    * 
+    * @serial  DEFAULT_USER
+    * @see #USER
+    */
     public static final String DEFAULT_USER = "root";
+    
+    /**
+    *
+    * <P>Default Database Password Registred
+    * 
+    * @serial  DEFAULT_PASSWORD
+    * @see #PASSWORD
+    */
     public static final String DEFAULT_PASSWORD = "";
+    
+    /**
+    *
+    * <P>Default Driver Location
+    * 
+    * <P>This driver refers to the latest JDBC (mysql-connector-java 8.0.22)
+    * 
+    * <P>if you use an older JDBC such as mysql-connector-java 5.1.48, then use {@link #OLD_DEFAULT_DRIVER}
+    * 
+    * @serial  DEFAULT_DRIVER
+    * @see #DRIVER
+    * @see #OLD_DEFAULT_DRIVER 
+    */
     public static final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
+    
+    /**
+    *
+    * <P> Default Driver Location
+    * 
+    * <P> This driver refers to older JDBCs as mysql-connector-java 5.1.48
+    *
+    * <P> if you use a newer JDBC, like mysql-connector-java 8.0.22, use {@link #OLD_DEFAULT_DRIVER}
+    *
+    * @serial  OLD_DEFAULT_DRIVER
+    * @see #DRIVER
+    * @see #DEFAULT_DRIVER 
+    */
+    public static final String OLD_DEFAULT_DRIVER = "com.mysql.jdbc.Driver";
+    
+    /**
+    *
+    * <P>Default Database Host
+    * 
+    * @serial  DEFAULT_HOST
+    * @see #HOST
+    */
     public static final String DEFAULT_HOST = "localhost";
+    
+    /**
+    *
+    * <P>Default Database Port
+    * 
+    * @serial  DEFAULT_PORT
+    * @see #PORT
+    */
     public static final String DEFAULT_PORT = "3306";
 
     
